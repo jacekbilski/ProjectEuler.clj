ebd534149831eba2ed93d5879e9da2a1c6f0f3b5
Jacek Bilski <jacek@bilski.tech>
2023-05-09 13:50:30 +0200

    Going back to Clojure = obligatory dependencies upgrade.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject project-euler.clj "0.1.0-SNAPSHOT"
   :description "Learning Clojure by solving Project Euler problems"
   :url "https://github.com/jacekbilski/ProjectEuler.clj"
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
